cf7b6820daa7af39ee0566478e3fea54d5cbc7f3
claireisabelle <claire.bourdale@gmail.com>
2018-02-13 10:16:36 +0100

    Initialisation plugin EVENT avec CMB2

---
+++ ga-cmb2.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Gourmet Artistry Metaboxes CMB2
+Plugin URI:
+Description: Adds Metaboxes with CMB2 to Gourtmet Artistry Site
+Version: 1.0
+Author: Claire Bourdalé - Arroweb
+Author URI: https://arroweb.net
+License: GLP2
+Licence URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
